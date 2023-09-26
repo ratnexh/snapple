411dbf9e1a493e0fc970d6de61ae5ef75fb59dd3
ratnexh <rkmaurya0709@gmail.com>
2023-09-26 16:21:32 +0530

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { useEffect, useState } from "react";
+import Loader from './components/Loader';
+
 interface Image {
   id: string,
   color: string,
@@ -23,9 +25,12 @@ export default function Home() {
   const [data, setData] = useState<Image[]>([]);
   const [search, setSearch] = useState('');
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
 
   const fetchData = async (s: any, p: number) => {
     try {
+      setLoading(true);
       const res = await fetch(`https://api.unsplash.com/search/photos?page=${p}&query=${s}&per_page=30`, {
         headers: {
           'Authorization': 'Client-ID 0HStgGiTz9-_10dWeN0PD7IzWMp4qPHrxYBcC-IFIBE'
@@ -33,32 +38,42 @@ export default function Home() {
       });
       const resData = await res.json()
       setData(resData.results)
+      setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   }
-
   useEffect(() => {
-    search === '' ? fetchData('purple', page) : fetchData(search, page)
+    setLoading(true);
+    if (timeoutId) clearTimeout(timeoutId);
+    const id = setTimeout(() => {
+      search === '' ? fetchData('purple', page) : fetchData(search, page)
+    }, 500);
+    setTimeoutId(id);
   }, [search, page])
 
   return (
-    <div className="container">
-      <div className="logo"><a href="/">PIXHUB.</a></div>
-      <input type="search" name="image-search" id="search" value={search} onChange={(e: any) => { setSearch(e.target.value) }} placeholder="Search images here..." />
-      <div className="cards">
-        {data.map((item: Image, index: number) => (
-          <div key={index} className={`item item-${index}`}>
-            <a href={item.urls.raw} target="_blank"> <img src={item.urls.small} alt={item.alt_description} /></a>
-          </div>
-        ))}
-      </div>
-      <div className="navigation">
-        {page >= 2 && <button onClick={() => { setPage(prevPage => prevPage - 1) }}>Previous Page</button>}
-        <button onClick={() => { setPage(nextPage => nextPage + 1) }}>Next Page</button>
-      </div>
-    </div>
+    <>
+      {loading ? <Loader /> : (
+        <div className="container">
+          <div className="logo"><a href="/">PIXHUB.</a></div>
+          <input type="search" name="image-search" id="search" value={search} onChange={(e: any) => { setSearch(e.target.value) }} placeholder="Search images here..." />
+          <>
+            <div className="cards">
+              {data.map((item: Image, index: number) => (
+                <div key={index} className={`item item-${index}`}>
+                  <a href={item.urls.raw} target="_blank"> <img src={item.urls.small} alt={item.alt_description} /></a>
+                </div>
+              ))}
+            </div>
+            <div className="navigation">
+              {page >= 2 && <button onClick={() => { setPage(prevPage => prevPage - 1) }}>Previous Page</button>}
+              <button onClick={() => { setPage(nextPage => nextPage + 1) }}>Next Page</button>
+            </div>
+          </>
+        </div>
+      )}
+    </>
   )
 }
-
-
