e0af60e8648d4dcd24fd39d7689236cf29ba04d8
ratnexh <rkmaurya0709@gmail.com>
2023-09-26 12:09:10 +0530

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,10 +21,11 @@ interface Image {
 }
 export default function Home() {
   const [data, setData] = useState<Image[]>([]);
+  const [search, setSearch] = useState('');
   const [page, setPage] = useState(1);
+
   const fetchData = async (s: any, p: number) => {
     try {
-
       const res = await fetch(`https://api.unsplash.com/search/photos?page=${p}&query=${s}&per_page=30`, {
         headers: {
           'Authorization': 'Client-ID 0HStgGiTz9-_10dWeN0PD7IzWMp4qPHrxYBcC-IFIBE'
@@ -37,30 +38,25 @@ export default function Home() {
     }
   }
 
-  const [search, setSearch] = useState('');
-  const handleSearch = (e: any) => {
-    e.preventDefault();
-    setSearch(e.target.value)
-    setTimeout(() => {
-      fetchData(search, page)
-    }, 2000)
-  }
-
   useEffect(() => {
-    fetchData('grey', page);
-  }, [])
+    // search === '' ? fetchData('purple', page) : fetchData(search, page)
+    fetchData(search, page)
+  }, [search, page])
 
   return (
     <div className="container">
-      <input type="search" name="image-search" id="search" value={search} onChange={handleSearch} placeholder="Write keyword to search..." />
+      <input type="search" name="image-search" id="search" value={search} onChange={(e: any) => { setSearch(e.target.value) }} placeholder="Write keyword to search..." />
       <div className="cards">
         {data.map((item: Image, index: number) => (
           <div key={index} className={`item item-${index}`}>
-            <a href={item.urls.full} target="_blank"> <img src={item.urls.small} alt={item.alt_description} /></a>
+            <a href={item.urls.raw} target="_blank"> <img src={item.urls.regular} alt={item.alt_description} /></a>
           </div>
         ))}
       </div>
+      {page >= 2 ? <button onClick={() => { setPage(prevPage => prevPage - 1) }}>Previous Page</button> : ''}
+      <button onClick={() => { setPage(nextPage => nextPage + 1) }}>Next Page</button>
     </div>
   )
 }
+
 
